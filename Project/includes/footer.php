f93df07f16c03311cce9a349bd0b33a899b30f42
Abdelrhman_313 <abdelrhmanyasser77@gmail.com>
2020-05-18 13:45:45 +0200

    firstCommit

---
+++ Project/includes/footer.php
@@ -0,0 +1,10 @@
+<section class="footer-section text-center">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   &copy; 2020 Library System | Designed by : SW2 Team
+               </div>
+
+           </div>
+       </div>
+   </section>
